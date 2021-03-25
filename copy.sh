991a25b0a39f990194e48b12b0b2578963397972
Matt-Pisini <pisini@usc.edu>
2021-03-24 20:40:18 -0700

    bash

--- copy.sh
+++ copy.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-DEST_PATH='/home/ee597/ns-3-allinone/ns-3-dev/scratch'
 SRC_FILE = "sim2.cc"
-cp $SRC_FILE $DEST_PATH/$SRC_FILE
+DEST_PATH='/home/ee597/ns-3-allinone/ns-3-dev/scratch${SRC_FILE}'
+
+cp $SRC_FILE $DEST_PATH
