fc97af518049c3ae0410fa42254006bb821a7aa4
Matt-Pisini <pisini@usc.edu>
2021-03-24 21:15:04 -0700

    bash

--- copy.sh
+++ copy.sh
@@ -5,5 +5,5 @@ DEST_PATH='/home/ee597/ns-3-allinone/ns-3-dev/scratch/'
 DEST_RUN='/home/ee597/ns-3-allinone/ns-3-dev/'
 COMMAND="./waf --run $SRC_FILE"
 cp $SRC_FILE $DEST_PATH
-cd DEST_RUN
-eval COMMAND
+cd $DEST_RUN
+eval $COMMAND
