aa0c3c84074b4b5c12d6f337df487f246f248214
Matt-Pisini <pisini@usc.edu>
2021-03-25 22:24:31 -0700

    script part 2

--- multiple_run.sh
+++ multiple_run.sh
@@ -2,9 +2,13 @@
 # INPUTS TO SCRIPT
 # 1) number of trials to average over
 
-RUN_SCRIPT='bash run.sh A node'
+RUN_SCRIPT_A_node='bash run.sh A node'
+RUN_SCRIPT_A_rate='bash run.sh A rate'
+RUN_SCRIPT_B_node='bash run.sh B node'
+RUN_SCRIPT_B_rate='bash run.sh B rate'
 
-for ((i=0; i<$1; i++))
+
+for ((i=0; i<5; i++))
 do
-    eval $RUN_SCRIPT $i
+    eval $RUN_SCRIPT_A_node $i
 done
