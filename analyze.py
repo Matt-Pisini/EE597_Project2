2c339c21a0d5af50d21a098e5b3a785689380f70
Matt-Pisini <pisini@usc.edu>
2021-03-25 17:36:22 -0700

    bash script + python script

---
+++ analyze.py
@@ -0,0 +1,56 @@
+import os
+import sys
+import matplotlib.pyplot as plt
+
+input_file_name, input_type = sys.argv
+input_file = open(input_file_name, 'r')
+input_file_lines = input_file.readlines()
+input_file.close()
+
+T_list= []
+N_list = []
+T_per_node_list = []
+DR_list = []
+
+for item in input_file_lines:
+    throughput, per_node_throughput, node_num, data_rate = item.split(' ')
+    if input_type == "node":
+        T_list.append(throughput)
+        N_list.append(node_num)
+        T_per_node_list.append(per_node_throughput)
+    elif input_type == "rate":
+        T_list.append(throughput)
+        T_per_node_list.append(per_node_throughput)
+        DR_list.append(data_rate)
+
+
+if input_type == "node":
+    plt.figure(1)
+    plt.plot(N_list, T_per_node_list)
+    plt.title("Saturation Throughput Per Node vs. Number of Nodes"
+    plt.xlabel("Number of Nodes")
+    plt.ylabel("Saturation Throughput Per Node")
+    plt.savefig("throughput_per_node.png")
+    plt.show()    
+    plt.figure(2)
+    plt.plot(N_list, T_list)
+    plt.title("Saturation Throughput vs. Number of Nodes"
+    plt.xlabel("Number of Nodes")
+    plt.ylabel("Saturation Throughput")
+    plt.savefig("throughput_node.png")
+    plt.show()
+elif input_type == "rate":
+    plt.figure(1)
+    plt.plot(DR_list, T_per_node_list)
+    plt.title("Saturation Throughput Per Node vs. Data Rate"
+    plt.xlabel("Date Rate")
+    plt.ylabel("Saturation Throughput Per Node")
+    plt.savefig("per_node_throughput_rate.png")
+    plt.show()
+    plt.figure(2)
+    plt.plot(DR_list, T_list)
+    plt.title("Saturation Throughput vs. Data Rate"
+    plt.xlabel("Data Rate")
+    plt.ylabel("Saturation Throughput")
+    plt.savefig("throughput_rate.png")
+    plt.show()
---
+++ run.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+# INPUTS TO SCRIPT
+# 1) input "A" or "B" meaning case A (CW = [1,1023]) or case B (CW = [63,127])
+# 2) input "node" or "rate" meaning vary nodes (E1) or vary rate (E2)
+
+SRC_FILE='sim2.cc'
+DEST_PATH='/home/ee597/ns-3-allinone/ns-3-dev/scratch/'
+SRC_PATH=$PWD
+DEST_RUN='/home/ee597/ns-3-allinone/ns-3-dev/'
+OUTPUT_FILE_RATE='vary_rate.txt'
+OUTPUT_FILE_NODE='vary_node.txt'
+HELP_OUTPUT=$'Input 1 = "A" or "B" meaning case A (CW = [1,1023]) or case B (CW = [63,127])
+Input 2 = "node" or "rate" meaning vary nodes (E1) or vary rate (E2)'
+PY_SCRIPT='analyze.py'
+# Command parameters
+FIXED_NUM_NODES='20'
+NUM_NODES='--N='
+FIXED_DATA_RATE='10'
+DATA_RATE='--DATA_RATE='
+COMMAND='./waf --run'
+CASE='--CASE='
+
+
+cp $SRC_FILE $DEST_PATH
+cd $DEST_RUN
+eval $COMMAND
+
+if [[ $1 != "A" && $1 != "B" ]]; then
+    echo "Incorrect input for 1st parameter."
+    echo "$HELP_OUTPUT"
+    exit
+fi
+
+if [[ $2 == "node" ]]; then
+    for ((i=5; i<=50; i += 5))
+    do
+        eval "$COMMAND \"$SRC_FILE $CASE$1 $OUTPUT_FILE$i $DATA_RATE$FIXED_DATA_RATE $NUM_NODES$i\"" >> $OUTPUT_FILE_NODE
+    done
+    OUTPUT_FILE=$OUTPUT_FILE_NODE
+elif [[ $2 == "rate" ]]; then
+    for ((i=5; i<=50; i += 5))
+    do
+        eval "$COMMAND \"$SRC_FILE $CASE$1 $OUTPUT_FILE$i $DATA_RATE$i $NUM_NODES$FIXED_NUM_NODES\"" >> $OUTPUT_FILE_RATE
+    done
+    OUTPUT_FILE=$OUTPUT_FILE_RATE
+else
+    echo "Incorrect input for 2nd parameter."
+    echo "$HELP_OUTPUT"
+    exit
+fi
+
+cp $OUTPUT_FILE $SRC_PATH
+cd $SRC_PATH
+eval "python $PY_SCRIPT $OUTPUT_FILE $2"
